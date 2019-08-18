9984aac740cad9e6ee77f3843e007930ca566268
Richard Russell <richardr39@gmail.com>
2019-08-18 23:51:39 +0100

    initial commit

---
+++ ClientDependencyInjectionDemo/ClientDependencyInjectionDemo/Controllers/ValuesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BookStore.Api.Client.Contracts;
+
+namespace ClientDependencyInjectionDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public ValuesController(IBooksClient booksClient)
+        {
+            _booksClient = booksClient;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<IEnumerable<string>> Get()
+        {
+            return await _booksClient.GetAllAsync();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        private readonly IBooksClient _booksClient;
+    }
+}
